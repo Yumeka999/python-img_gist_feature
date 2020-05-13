2f80eae1c7ee740c4f013633fe18d58b552a258e
kalafinaian <kalafinaian@qq.com>
2020-05-14 07:37:15 +0800

    update is_bpg_img

--- img_gist_feature/utils_img.py
+++ img_gist_feature/utils_img.py
@@ -381,12 +381,15 @@ def img_resize_win(np_img_in, n_max, n_limit_ratio):
 
 
 # 判断是否为正确的bpg图片
-def is_bpg_img(s_img_in_url, run_logger=None):
+def is_bpg_img(s_img_in_url, run_logger=None, b_print=False):
     if not os.path.exists(s_img_in_url) or not os.path.isfile(s_img_in_url):
         return -1
 
     if not s_img_in_url.endswith(".bpg"):
-        run_logger and run_logger.warning("not endwith .bpg")
+        s_msg = "not endwith .bpg"
+        run_logger and run_logger.warning(m_msg)
+        b_print and print(m_msg)
+
         return 1
 
     with open(s_img_in_url, "rb") as fp:
@@ -398,8 +401,10 @@ def is_bpg_img(s_img_in_url, run_logger=None):
             # print("%#x" % n_byte_1)
             # print("%#x" % n_byte_2) 
             break
-        
-    run_logger and run_logger.warning("not right bpg")
+    
+    m_msg = "not right bpg"
+    run_logger and run_logger.warning(m_msg)
+    b_print and print(m_msg)
     return -1
 
 
