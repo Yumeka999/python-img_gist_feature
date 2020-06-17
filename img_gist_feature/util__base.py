3c4eb4b1553814ae6caafec346b4ec61e3e2e871
kalafinaian <kalafinaian@qq.com>
2020-06-17 08:45:03 +0800

    update

--- img_gist_feature/util__base.py
+++ img_gist_feature/util__base.py
@@ -86,7 +86,7 @@ def rm_file_dir(s_in_url, run_logger=None, b_print=False):
             b_print and print(s_msg)
             return 1
     except Exception as e:
-         s_msg = 'Err: cant'' remove %s' % (s_in_url, str(e))
+         s_msg = 'Err: cant'' remove %s, %s' % (s_in_url, str(e))
          run_logger and run_logger.error(s_msg)
          b_print and print(s_msg)
          return -1
@@ -103,14 +103,17 @@ def get_usable_str(s_in):
 
 # Get all cos sim
 #@numba.autojit
-def get_all_cos_sim(np_A ,np_B, np_B_L2 = None):
+def get_all_cos_sim(np_A ,np_B, np_B_L2 = None, run_logger=None, b_print=False):
     n_num = np_B.shape[0]
     
     t1 = time.clock()  
     np_A_L2 = np.linalg.norm(np_A)
     np_A_L2 = np.tile(np_A_L2, (1, n_num))   
     t2 = time.clock()
-    print('A L2 time',t2 - t1)
+
+    s_msg = "A L2 time %.3f" % (t2-t1)
+    run_logger and run_logger.info(s_msg)
+    b_print and print(s_msg)
     
     if np_B_L2 is None:
         t1 = time.clock()  
