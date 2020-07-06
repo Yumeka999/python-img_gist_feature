8cdf590dcdbdc9ee258175099d6aa965e02d1b8a
kalafinaian <kalafinaian@qq.com>
2020-07-06 08:06:00 +0800

    update

--- img_gist_feature/util__img.py
+++ img_gist_feature/util__img.py
@@ -436,14 +436,8 @@ def get_histeq_img(np_img_in, run_log=None, b_print=False):
 # Compute ssim
 def get_ssim(np_img_A, np_img_B, run_log=None, b_print=False):
     # 必须是灰度图
-    if len(np_img_A.shape) == 3:
-        s_msg = "input A is not gray image"
-        run_log and run_log.erro(s_msg)
-        b_print and b_print(s_msg)
-        return -1.0
-
-    if len(np_img_B.shape) == 3:
-        s_msg = "input B is not gray image"
+    if len(np_img_A.shape) == 3 or len(np_img_B.shape) == 3:
+        s_msg = "input A or B is not gray image, A:%s, B:%s" % (str(np_img_A.shape), str(np_img_B.shape))
         run_log and run_log.erro(s_msg)
         b_print and b_print(s_msg)
         return -1.0
