dc7f16461926c609c087d4f55e8ee5d1787814ff
kalafinaian <kalafinaian@qq.com>
2020-10-22 07:45:35 +0800

    update

--- img_gist_feature/util__base.py
+++ img_gist_feature/util__base.py
@@ -22,7 +22,6 @@ def recur_mkdir(s_path, run_log=None, b_print=False):
             n_ret, s_msg = -1, "mkdir err: %s" % str(e)    
     run_log and run_log.warning(s_msg)
     b_print and print(s_msg)
-
     return n_ret
 
 '''
@@ -34,18 +33,15 @@ def cp_file_dir(s_in_url, s_out_url, run_log=None, b_print=False):
             s_msg = 'Err in rm_file_dir'
             run_log and run_log.error(s_msg)
             b_print and print(s_msg)
-
             return -1
     cp_func = shutil.copyfile if os.path.isfile(s_in_url) else shutil.copytree
-
     try:
         cp_func(s_in_url, s_out_url) # Copy a old file to tmp dir 
         return 0     
     except Exception as e:
         s_msg = 'Err: cant''t copy %s to %s, %s' % (s_in_url, s_out_url, str(e))
         run_log and run_log.error(s_msg)
-        b_print and print(s_msg)
-        
+        b_print and print(s_msg)       
         return -1
    
 '''      
@@ -54,7 +50,6 @@ Move a file or directory
 def mv_file_dir(s_in_url, s_out_url, run_log=None, b_print=False):
     try: 
         shutil.move(s_in_url, s_out_url) # using shutil.move
-
         return 0
     except Exception as e:
         s_msg = 'Err: cant''t copy %s to %s, %s' % (s_in_url, s_out_url, str(e))
@@ -70,19 +65,16 @@ def rm_file_dir(s_in_url, run_log=None, b_print=False):
     try:
         if os.path.exists(s_in_url): # is a directory
             shutil.rmtree(s_in_url) 
-
             return 0
         else:
             s_msg = 'Err: not exists %s' % (s_in_url)
             run_log and run_log.error(s_msg)
             b_print and print(s_msg)
-
             return 1
     except Exception as e:
         s_msg = 'Err: cant'' remove %s, %s' % (s_in_url, str(e))
         run_log and run_log.error(s_msg)
         b_print and print(s_msg)
-
         return -1
 
 '''
@@ -93,8 +85,7 @@ def get_usable_str(s_in):
     s_unvalid = '<>,\/|,:.,''",*,?\t\r\n'
     for ch in s_unvalid:
         s_tmp = s_tmp.replace(ch,'')
-    s_tmp = s_tmp.replace(u'\u3000','')
-    
+    s_tmp = s_tmp.replace(u'\u3000','')   
     return s_tmp
 
 '''
