be607cb6210bc5e66dde84e49786ea5ac21bc7fd
kalafinaian <kalafinaian@qq.com>
2020-11-04 20:53:36 +0800

    update

--- img_gist_feature/util__base.py
+++ img_gist_feature/util__base.py
@@ -28,13 +28,13 @@ def recur_mkdir(s_path, run_log=None, b_print=False):
 Copy a file or directory
 '''
 def cp_file_dir(s_in_url, s_out_url, run_log=None, b_print=False):
-    if os.path.exists(s_out_url):  
-        if rm_file_dir(s_out_url) < 0:
-            s_msg = 'Err in rm_file_dir'
-            run_log and run_log.error(s_msg)
-            b_print and print(s_msg)
-            return -1  
     try:
+        if os.path.exists(s_out_url):  
+            if rm_file_dir(s_out_url) < 0:
+                s_msg = 'Err in rm_file_dir'
+                run_log and run_log.error(s_msg)
+                b_print and print(s_msg)
+                return -1  
         cp_func = shutil.copyfile if os.path.isfile(s_in_url) else shutil.copytree
         cp_func(s_in_url, s_out_url) # Copy a old file to tmp dir 
         return 0     
