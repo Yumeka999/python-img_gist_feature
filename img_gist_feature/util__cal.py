d28710384d1c57cab4a03306e6af3c9081423617
kalafinaian <kalafinaian@qq.com>
2020-11-21 08:11:57 +0800

    update

--- img_gist_feature/util__cal.py
+++ img_gist_feature/util__cal.py
@@ -4,8 +4,9 @@ import os
 import time
 import numpy as np
 
-# Get all cos sim
-#@numba.autojit
+'''
+Get all cos sim
+'''
 def get_all_cos_sim(np_A ,np_B, np_B_L2 = None, run_logger=None, b_print=False):
     n_num = np_B.shape[0] 
     np_A_L2 = np.linalg.norm(np_A)
@@ -16,7 +17,10 @@ def get_all_cos_sim(np_A ,np_B, np_B_L2 = None, run_logger=None, b_print=False):
     np_cos_sim = np_inner/(np_A_L2 * np_B_L2)
     np_cos_sim = 0.5 + 0.5 * np_cos_sim
     return np_cos_sim
-  
+
+'''
+get l2 norm of a vector
+'''  
 def np_l2norm(np_x, run_log=None, b_print=False):
     if len(np_x.shape) > 2:
         return -1
@@ -32,5 +36,8 @@ def np_l2norm(np_x, run_log=None, b_print=False):
     np_x_L2Norm[np_where_are_nans] = 0.0
     return np_x_L2Norm
 
+'''
+get cos similarity
+'''
 def get_cos_sim(np_A, np_B):
     return np.inner(np_A, np_B)/(np.linalg.norm(np_A) * np.linalg.norm(np_B))
