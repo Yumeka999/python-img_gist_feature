3fe3ee23a0e2413ee1ef6e65c84f01f40afdf9df
kalafinaian <kalafinaian@qq.com>
2020-11-23 08:02:22 +0800

    update

--- img_gist_feature/util__cal.py
+++ img_gist_feature/util__cal.py
@@ -14,18 +14,15 @@ def get_all_cos_sim(np_A ,np_B, np_B_L2 = None, run_logger=None, b_print=False):
     if np_B_L2 is None:
         np_B_L2 = np.linalg.norm(np_B, axis = 1)
     np_inner = np_A.dot(np_B.T)    
-    np_cos_sim = np_inner/(np_A_L2 * np_B_L2)
-    np_cos_sim = 0.5 + 0.5 * np_cos_sim
+    np_cos_sim = 0.5 + 0.5 * np_inner/(np_A_L2 * np_B_L2)
     return np_cos_sim
 
 '''
 get l2 norm of a vector
 '''  
 def np_l2norm(np_x, run_log=None, b_print=False):
-    if len(np_x.shape) > 2:
-        return -1
-    elif len(np_x.shape) == 1:
-        np_x = np_x[:, np.newaxis].T             
+    if len(np_x.shape) > 2: return -1
+    elif len(np_x.shape) == 1: np_x = np_x[:, np.newaxis].T             
     n_feat_num = np_x.shape[1]  # the feature number of input      
     np_x_L2 = np.linalg.norm(np_x, axis = 1)
     np_x_L2 = np_x_L2[:, np.newaxis]
