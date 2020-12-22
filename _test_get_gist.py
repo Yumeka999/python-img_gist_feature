c9fcc14f2c37de3c5a93bf11802c7959d96cb882
kalafinaian <kalafinaian@qq.com>
2020-12-22 08:27:09 +0800

    update

--- _test_get_gist.py
+++ _test_get_gist.py
@@ -1,6 +1,5 @@
 import cv2
 import sys
-
 sys.path.append("./img_gist_feature/")
 
 from utils_gist import *
@@ -15,18 +14,13 @@ np_gist = gist_helper.get_gist_vec(np_img, mode="rgb")
 print("shape ", np_gist.shape)
 print("noly show 10dim", np_gist[0,:10], "...")
 print()
-
-
 print("convert rgb image")
 np_gist = gist_helper.get_gist_vec(np_img, mode="rgb")
 print("shape ", np_gist.shape)
 print("noly show 10dim", np_gist[0,:10], "...")
 print()
-
-
 print("convert gray image")
 np_gist = gist_helper.get_gist_vec(np_img, mode="gray")
 print("shape ", np_gist.shape)
 print("noly show 10dim", np_gist[0,:10], "...")
 print()
-
