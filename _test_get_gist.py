e5aa8bb4f3baea2ab4306da6e6d818d9a9ed57c0
kalafinaian <kalafinaian@qq.com>
2020-12-22 08:27:30 +0800

    update

--- _test_get_gist.py
+++ _test_get_gist.py
@@ -6,9 +6,7 @@ from utils_gist import *
 
 s_img_url = "./test/A.jpg"
 gist_helper = GistUtils()
-
 np_img = cv2.imread(s_img_url, -1)
-
 print("default: rgb")
 np_gist = gist_helper.get_gist_vec(np_img, mode="rgb")
 print("shape ", np_gist.shape)
